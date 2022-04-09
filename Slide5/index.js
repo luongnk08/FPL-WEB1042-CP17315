b2574aa0975fcca9f7579190b80578536d2388b7
luongnk08 <nn1471998@gmail.com>
2022-04-09 20:11:11 +0700

    slide 78

--- Slide5/index.js
+++ Slide5/index.js
@@ -10,27 +10,27 @@ function showDog() {
   elements[0].style.background = "#3333CC";
   elements[1].style.background = "#CC0066";
 
-  // console.log(document.getElementsByName("nodeName"));
+  console.log(document.getElementsByName("nodeName"));
 
-  // var testNode = elements.item(0);
+  var testNode = elements.item(0);
 
-  // dAnhElement.innerHTML = "<img src='images/dog.jpg' />";
+  dAnhElement.innerHTML = "<img src='images/dog.jpg' />";
 
-  // var imgNode = document.createElement("img");
+  var imgNode = document.createElement("img");
 
-  // imgNode.setAttribute("src", "images/dog.jpg");
+  imgNode.setAttribute("src", "images/dog.jpg");
 
-  // var testNode1 = testNode.cloneNode(true);
-  // var testNode2 = testNode.cloneNode(true);
+  var testNode1 = testNode.cloneNode(true);
+  var testNode2 = testNode.cloneNode(true);
 
-  // console.log(testNode1);
+  console.log(testNode1);
 
-  // dAnhElement.append(testNode, imgNode, testNode1, testNode2);
+  dAnhElement.append(testNode, imgNode, testNode1, testNode2);
 
-  // imgNode.style.transition = "0.2s all ease";
+  imgNode.style.transition = "0.2s all ease";
 
-  // let mL = 0;
-  // setInterval(() => {
-  //   imgNode.style.marginLeft = `${mL++}px`;
-  // }, 5);
+  let mL = 0;
+  setInterval(() => {
+    imgNode.style.marginLeft = `${mL++}px`;
+  }, 5);
 }
---
+++ Slide7_8/index.module.js
@@ -0,0 +1,29 @@
+var app = angular.module("myApp", []);
+
+app.controller("indexController", function ($scope) {
+  $scope.name = "I'm FPoly";
+
+  $scope.inHoa = function () {
+    $scope.name = $scope.name.toUpperCase();
+  };
+
+  $scope.giaTien = 100000;
+  $scope.soLuong = 1;
+  $scope.thanhTien = $scope.giaTien * $scope.soLuong;
+
+  // kiểm tra tính hợp lệ
+  $scope.giaTienHL = $scope.giaTien > 0;
+  $scope.soLuongHL = $scope.soLuong > 0;
+
+  $scope.tinhTien = function () {
+    // kiểm tra tính hợp lệ
+    $scope.giaTienHL = $scope.giaTien > 0;
+    $scope.soLuongHL = $scope.soLuong > 0;
+
+    if ($scope.giaTien > 0 && $scope.soLuong > 0) {
+      $scope.thanhTien = $scope.giaTien * $scope.soLuong;
+    } else {
+      $scope.thanhTien = 0;
+    }
+  };
+});
